5483814aa08c2976b2a9514437f3bdbf0369e10d
Robert Capps <rocapp@gmail.com>
2022-11-28 00:08:24 -0800

    build fixes

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+rm dist/desmos2python-*{.tar.gz,.whl}
 python3 -m pip install -r build-requirements.txt --upgrade
 python3 -m build
--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python3 -m twine upload --repository testpypi dist/*
+python3 -m twine upload dist/*$(git tag | tail -1)*
