14bca8c9d1f62655844141f5e61cfbed9216fdae
Robert Capps <rocapp@gmail.com>
2022-11-28 05:41:27 -0800

    small change to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,6 @@
 old_tag=$(python3 -c $'import setuptools_scm as scm; print("{}".format(scm.version_from_scm(".").tag.base_version))')
 tag=$(python3 -c $'import setuptools_scm as scm; print(".".join(scm.get_version().split(".")[:3]))')
 git tag "v$tag"
-git push --tags
-twine upload dist/*
+twine upload dist/* &&
+    git push --tags &&
+    git push --all
