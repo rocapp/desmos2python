81a55276d40d0669446405bf840b25259a2971a9
Robert Capps <rocapp@gmail.com>
2022-11-27 23:57:09 -0800

    build fixes

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     use_scm_version={
-        'write_to': '_version.py',
+        'write_to': 'desmos2python/_version.py',
         'write_to_template': '__version__ = "{version}"',
     },
     setup_requires=['setuptools_scm'],
