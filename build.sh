b2c683ab2ebb6bdd23f12ea45b20ac9b7a6b6226
Robert Capps <rocapp@gmail.com>
2022-11-27 23:13:00 -0800

    updates to build logic

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+python3 -m pip install -r build-requirements.txt --upgrade
+python3 -m build
--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-python3 -m pip install -r build-requirements.txt --upgrade
-python3 -m build
 python3 -m twine upload --repository testpypi dist/*
