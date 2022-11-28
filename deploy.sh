09e4e8ee7013948793d2943eb2b81a48dd3af932
Robert Capps <rocapp@gmail.com>
2022-11-28 00:55:58 -0800

    fixed deployment

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-tag=$(python3 -c $'import setuptools_scm as scm; print("{}".format(scm.version_from_scm(".").tag.base_version))')
+old_tag=$(python3 -c $'import setuptools_scm as scm; print("{}".format(scm.version_from_scm(".").tag.base_version))')
+tag=$(python3 -m setuptools_scm)
 git tag "v$tag"
+git push --tags
 twine upload dist/*$tag*
