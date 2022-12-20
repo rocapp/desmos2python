81f4ad1523ae7af8f78cca24bba77711fce22171
Robbie Capps <rcapps@teladochealth.com>
2022-12-20 10:33:00 -0800

    (todo: follow up on pdoc convert2plain method... likely the best option for converting a system of equations) updated pdoc.py

--- desmos2python/pdoc.py
+++ desmos2python/pdoc.py
@@ -1,6 +1,16 @@
 """desmos2python pandoc wrapper methods"""
 import os
+import pypandoc as pdoc
 
 
-def convert(s, opts=''):
+def convert2html(s, opts=''):
+    """convert generic input (e.g., latex) to pandoc HTML format"""
     return os.popen(f"echo '{s}' | pandoc {opts}").read()
+
+
+def convert2plain(estr, clean_ws=True):
+    """convert generic input (e.g., latex) to pandoc plain format"""
+    plain = pdoc.convert_text(source=estr, to='plain', format='latex')
+    if clean_ws is True:
+        plain = plain.strip().replace('\n', '').replace('\r', '')
+    return plain
