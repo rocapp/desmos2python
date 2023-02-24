320be6e3be9b2dfd8a59335b04afdab32c11de54
Robbie Capps <rocapp@gmail.com>
2023-02-24 06:26:40 -0800

    fixed prebuild

--- scripts/prebuild.sh
+++ scripts/prebuild.sh
@@ -8,9 +8,6 @@ SCRIPTS_DIR=$(SCRIPTS_DIR=$(dirname "$0") && bash -c "cd \"$SCRIPTS_DIR\" && pwd
 #: install build dependencies
 python -m pip install -r $SCRIPTS_DIR/requirements.txt -r $SCRIPTS_DIR/../requirements/build-requirements.txt
 
-#: render repo helper config
-$SCRIPTS_DIR/render-repo-helper-yaml
-
 #: fix readme RST
 python $SCRIPTS_DIR/../fix_readme.py
 
